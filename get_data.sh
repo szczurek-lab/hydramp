4d456e6cc5959b641a95dbc592a710e8e70a64b7
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2024-07-25 13:09:51 +0200

    fix: remove typo from data access script

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,7 @@
 # environment specified at ./data_setup
 python3 data_setup/download_data.py
 
-unzip unzip direct_download/models.zip
-unzip unzip direct_download/data.zip
-unzip unzip direct_download/results.zip
-unzip unzip direct_download/wheels.zip
+unzip direct_download/models.zip
+unzip direct_download/data.zip
+unzip direct_download/results.zip
+unzip direct_download/wheels.zip
