6590d2f4c2963f25d30669052a4c4a857e0e7279
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2024-11-26 22:25:40 +0100

    chore: update linkt to the gdrive

--- data_setup/download_data.py
+++ data_setup/download_data.py
@@ -1,5 +1,5 @@
 import gdown
 
-link = "https://drive.google.com/drive/folders/1IwJjwSKpXYWWALKXM8nghqmlSvyu3BYq"
+link = "https://drive.google.com/drive/folders/1krim1ugqNDmgmHZCFSOvmynWxCSzyOto"
 target_dir = "downloaded_data_zips"
 gdown.download_folder(link, output=target_dir)
